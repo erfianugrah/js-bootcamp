0d89c65ce118e8da136979dc4212784cd1a0cd38
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-13 10:16:23 +0800

    Challenge 3

---
+++ basics/boolean.js
@@ -0,0 +1,17 @@
+// === - equality
+// !== - no equal
+// < less than
+// > more than
+// >= more than or equal to
+// <= less than or equal to
+
+let temp = 31
+let isFreezing = temp < 32
+// console.log(isFreezing)
+
+// Challenge 3
+let age = 27
+let isChild = age <= 7
+let isSenior = age >= 65
+console.log(isChild)
+console.log(isSenior)
