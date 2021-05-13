a68a5dda9caa5ae4a0825be1133dd34372e3db9c
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-13 09:06:42 +0800

    Strings

---
+++ basics/strings.js
@@ -0,0 +1,12 @@
+let firstName = 'Erfi'
+let lastName = 'Anugrah'
+let fullName = `${firstName}${' '}${lastName}`
+
+console.log(fullName)
+
+// Challenge 1
+let city = 'Jurong'
+let country = 'Singapore'
+let location = `${city}${','}${' '}${country}`
+
+console.log(location)
