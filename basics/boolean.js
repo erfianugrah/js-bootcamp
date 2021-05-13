1a1013ca142cd7bcdc7098c84f2eebcc94eace05
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-13 11:59:50 +0800

    If statements

--- basics/boolean.js
+++ basics/boolean.js
@@ -9,9 +9,19 @@ let temp = 31
 let isFreezing = temp < 32
 // console.log(isFreezing)
 
+if (temp <= 32) {
+    console.log('It is freezing outside!')
+} else if (temp >= 110) {
+    console.log('It is way too hot outside!')
+} else console.log('Seems fine')
 // Challenge 3
 let age = 27
-let isChild = age <= 7
+/*let isChild = age <= 7
 let isSenior = age >= 65
 console.log(isChild)
-console.log(isSenior)
+console.log(isSenior)*/
+if (age <= 7) {
+    console.log('Child Pricing!')
+} else if (age >= 65) {
+    console.log('Senior discoung')
+} else console.log('You\'re SOL')
