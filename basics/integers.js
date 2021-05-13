26efee9051550e5aed9f9d0a73ef12db8e9b70c6
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-13 09:29:27 +0800

    Integers

---
+++ basics/integers.js
@@ -0,0 +1,13 @@
+let x = 11
+let num = (x + 1) * 2
+console.log(num)
+
+let age = 27
+let dogYears = (age + 1) / 7
+console.log(dogYears)
+
+// Challenge 2
+let studentScore = 90
+let maxScore = 100
+let percent = (studentScore / maxScore) * 100
+console.log(percent+'%')
