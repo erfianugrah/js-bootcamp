a001e153b08a321a9f1b00304f6e81052b3a30d5
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-14 14:35:20 +0800

    Multiple Arguments

---
+++ js-functions/arguments.js
@@ -0,0 +1,22 @@
+let add = function (x, y, z) {
+    return x + y + z
+}
+
+let result = add(11, 12, 14)
+console.log(result)
+
+// Default Arguments
+let getScoreText = function(name = 'Anonymous', score = 0) {
+    return 'Name ' + name + ' - Score: ' + score
+}
+
+let scoreText = getScoreText()
+console.log(scoreText)
+
+// Challenge
+let getTip = function(total, tipPercent = 0.2) {
+    return 'Total: ' + total * tipPercent
+}
+
+let bill = getTip(300, 0.1)
+console.log(bill)
