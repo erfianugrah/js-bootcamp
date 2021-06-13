78a3e45784e13d6838e8d98f08f0666b2214fdc1
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-06-13 09:25:47 +0800

    Todo filter challenge

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -11,25 +11,31 @@ const todos = [
     }
 ]
 
-const incompleteTodos = todos.filter(function (todo) {
-    return !todo.completed
-})
+const filters = {
+    searchText: ''
+}
 
-const summary = document.createElement('h2')
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
+const renderTodos = function (todo, thing) {
+        const filteredTodos = todo.filter( function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+        const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+        document.querySelector('#todos').innerHTML = ''
 
-todos.forEach(function (todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.thing
-    document.querySelector('body').appendChild(p)
-})
+        const summary = document.createElement('h2')
+        summary.textContent = `You have ${incompleteTodos.length} todos left`
+        document.querySelector('#todos').appendChild(summary)
 
+        filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.thing
+        document.querySelector('#todos').appendChild(p)
+    })
+}
 
-// Print Summmary - you have 2 todos left (p element)
-// Add a p for each to do above (use text value)
-
-// Listen for new todo creation
+renderTodos(todo, filters)
 
 document.querySelector('#add-todo').addEventListener('click', function (e) {
     console.log('Did this work?')
@@ -38,4 +44,9 @@ document.querySelector('#add-todo').addEventListener('click', function (e) {
 
 document.querySelector('#new-todo').addEventListener('input', function (e) {
     console.log(e.target.value)
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todo, filters)
 })
