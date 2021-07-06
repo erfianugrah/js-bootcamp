dda6f556a2d250ee1460bebcec237cda86a279d2
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-07-06 12:42:19 +0800

    Refactoring

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,36 +1,8 @@
-let todos = []
+const todos = getSavedTodos()
 
 const filters = {
     searchText: '',
     hideCompleted: false
-}
-
-const todosJSON = localStorage.getItem('todos')
-if (todosJSON !== null) {
-    todos = JSON.parse(todosJSON)
-}
-
-const renderTodos = function (todo, thing) {
-        const filteredTodos = todo.filter( function (todo) {
-            const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-            const hideCompletedmatch = !filters.hideCompleted || !todo.completed     
-            return searchTextMatch && hideCompletedmatch
-    })
-
-        const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed
-    })
-        document.querySelector('#todos').innerHTML = ''
-
-        const summary = document.createElement('h2')
-        summary.textContent = `You have ${incompleteTodos.length} todos left`
-        document.querySelector('#todos').appendChild(summary)
-
-        filteredTodos.forEach(function (todo) {
-        const p = document.createElement('p')
-        p.textContent = todo.thing
-        document.querySelector('#todos').appendChild(p)
-    })
 }
 
 renderTodos(todos, filters)
@@ -46,7 +18,7 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
         thing: e.target.elements.thing.value,
         completed:false
     })
-    localStorage.setItem('todos', JSON.stringify(todos))
+    saveTodos(todos)
     renderTodos(todos, filters)
     e.target.elements.thing.value = ''
 })
